037aa8bf17e143d19c65d0a35044ce410d8dc42d
Maahirsoni19 <maahirsoni19@gmail.com>
2026-01-20 15:10:39 +0530

    Add student struct and display functionality

---
+++ 2-1 - struct student display.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct Student
+{
+    char name[10];
+    int rn;
+    char address[100];
+};
+
+int main(){
+    printf("Enter the values as asked: \n");
+    struct Student s;
+    printf("Student Name: ");
+    fgets(s.name, sizeof(s.name), stdin);
+    printf("Roll Number: ");
+    scanf(" %d", &s.rn);
+    getchar();
+    printf("Address: ");
+    fgets(s.address, sizeof(s.address), stdin);
+
+    printf("The Data you entered is as follows: \n");
+    printf("Name : %s", s.name);
+    printf("Roll Number: %d\n", s.rn);
+    printf("Address: %s", s.address);
+
+    return 0;
+}
