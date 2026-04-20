84ec00c668f69a4e65b2e9adc869acd985d9e6ae
Maahir Soni <maahirsoni19@gmail.com>
2026-04-21 03:00:02 +0530

    Create 9-3 - leaf nodes.c

---
+++ 9-3 - leaf nodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = val;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Count leaf nodes
+int countLeaf(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeaf(root->left) + countLeaf(root->right);
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    int result = countLeaf(root);
+
+    printf("Leaf nodes = %d\n", result);
+
+    return 0;
+}
